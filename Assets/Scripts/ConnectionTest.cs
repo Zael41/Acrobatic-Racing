ffda98bc6b2a0236fd658b2d05c082d8d66e18b5
Zael41 <zaelgaming@gmail.com>
2022-10-13 22:28:34 +0200

    photon import + testing
    in case i want to make the game online

---
+++ Assets/Scripts/ConnectionTest.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class ConnectionTest : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.GameVersion = "0.1";
+
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Conectando al server master");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Conectando al server maestro");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("Fallo al conectar");
+    }
+}
---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public int roomNumber;
+
+    public void CrearRoom()
+    {
+        Debug.Log("Creando sala nueva");
+
+        PhotonNetwork.JoinOrCreateRoom("Sala no." + roomNumber, new RoomOptions() { MaxPlayers = 2 }, TypedLobby.Default);
+
+        Debug.Log("Sala creada");
+    }
+
+    public void UnirseRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+        Debug.Log("Unido a una sala aleatoria");
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Fallo al crear la sala");
+
+        CrearRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Fallo al unirse a la sala, se creara una nueva");
+
+        CrearRoom();
+    }
+}
